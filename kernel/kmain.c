4856f51a180c8a3b510a840cbf190264ef8e2f12
lotsandlotsofrobots <lotsandlotsofrobots@github.com>
2015-11-16 22:27:53 -0600

    Updated main to use com1; added newlines to console printing.

--- kernel/kmain.c
+++ kernel/kmain.c
@@ -1,25 +1,46 @@
 #include "io.h"
+#include "serial.h"
 
-void initConsole()
+void init_console()
 {
-    setForegroundColor(0x4);
-    setBackgroundColor(0x15);
-    setConsolePosition(0,0);
+    clear_screen();
+    set_foreground_color(CONSOLE_RED);
+    set_background_color(CONSOLE_GREEN);
+    set_console_position(0,0);
 }
+
+
+void init_debug_port()
+{
+    #define DEBUG_COM 1
+    set_serial_baud_rate(DEBUG_COM, 115200);
+    set_serial_line_control_settings(DEBUG_COM, 8, false, 1);
+    set_serial_fifo_settings(DEBUG_COM, SERIAL_FIFO_TRIGGER_14b_56bW, true);
+    set_rts(DEBUG_COM);
+}
+
 
 int main()
 {
-    initConsole();
-    clearscreen();
-    //printc(0, 0, 'a', FB_GREEN, FB_GREEN);
-    printf("hello\0");
-    printf("This is a test\0");
+    init_console();
+    clear_screen();
+    
+    printf("hello\n\0");
+    printf("This is a test\n\0");
 
-    //printc(0, 1, 'b', FB_GREEN, FB_BLACK);
-    //printc(0, 2, 'c', FB_GREEN, FB_BLACK);
-    //printc(0, 0, 'z', FB_GREEN, FB_BLACK);
-    //clearscreen();
-    //writechar();
+    init_debug_port();
+    
+    printf("set up the serial port\n\0");
+
+    serial_write(DEBUG_COM,'a');
+
+    printf("serial_write done\n\0");
+
+    char c[64];
+//uint8_t serial_read(uint8_t com, char *buffer, uint8_t buffersize);
+    uint8_t bytes_read = serial_read(DEBUG_COM, c, 64);
+    printf("Bytes read:\n\0");
+    printf(bytes_read);
 
     while ( 1 ) {
 	
